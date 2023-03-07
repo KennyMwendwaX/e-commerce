c971338c277263c9fe4dc8bef6cb2e72bd45e536
IconicDexter <kennymwendwa67@gmail.com>
2023-03-07 13:15:52 +0300

    commit 3

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,7 +5,7 @@ export default function Cart() {
   const { cartItems, cartQuantity } = useCart()
 
   return (
-    <div className="container px-5 py-20 mx-auto">
+    <div className="container px-5 py-20 pb-5 mx-auto">
       <h1 className="font-light">Cart {cartQuantity}</h1>
       <CartComponent />
     </div>
