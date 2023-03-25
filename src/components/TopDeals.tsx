8a8ac50f63bc9585d4e2398eb049a3dd7b9782c2
IconicDexter <kennymwendwa67@gmail.com>
2023-03-25 22:42:43 +0300

    commit

--- src/components/TopDeals.tsx
+++ src/components/TopDeals.tsx
@@ -29,24 +29,48 @@ export default function TopDeals() {
         </SwiperSlide>
         <SwiperSlide>
           <img className="cursor-pointer" src="/item-img/macbook.jpg" />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -5%
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="cursor-pointer"
+            src="/category-img/gaming-console.webp"
+          />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -10%
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="cursor-pointer" src="/category-img/footwear.jpg" />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -10%
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="cursor-pointer" src="/category-img/desktop.jpeg" />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -10%
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <img className="cursor-pointer" src="/item-img/gkmc.webp" />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -10%
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <img className="cursor-pointer" src="/item-img/samsung-tv.jpeg" />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -10%
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <img className="cursor-pointer" src="/item-img/rayban.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="cursor-pointer" src="/item-img/madvillainy.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="cursor-pointer" src="/item-img/madvillainy.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="cursor-pointer" src="/item-img/madvillainy.jpg" />
+          <div className="absolute top-2 right-1 bg-red-100 text-red-600 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+            -10%
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
