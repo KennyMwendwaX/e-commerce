4d9c24964fce92e7ee2f3bab80997833bb7ca591
IconicDexter <kennymwendwa67@gmail.com>
2023-06-16 08:46:10 +0300

    Fixed item/[id] api endpoint

--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -5,5 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "GET")
+    return res.status(405).json({ message: "Method Not Allowed" });
   res.status(200).json(items);
 }
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -17,7 +17,7 @@ import { useRouter } from "next/router";
 export default function Item() {
   const { addToCart, getItemQuantity } = useCart();
   const router = useRouter();
-  const id = router.query;
+  const { id } = router.query;
   const [item, setItem] = useState<ItemTypes | null>(null);
 
   useEffect(() => {
@@ -35,44 +35,46 @@ export default function Item() {
 
   return (
     <>
-      <div className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+      <div className="body-font overflow-hidden text-gray-600">
+        <div className="container mx-auto px-5 py-24">
+          <div className="mx-auto flex flex-wrap lg:w-4/5">
             <Image
               alt="ecommerce"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              width={200}
+              height={256}
+              className="h-64 w-full rounded object-cover object-center lg:h-auto lg:w-1/2"
               src={item.imgUrl}
             />
-            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h2 className="inline-block py-1 px-2 rounded bg-indigo-100 text-indigo-600 text-xs font-medium tracking-widest dark:bg-gray-700 dark:text-indigo-500">
+            <div className="mt-6 w-full lg:mt-0 lg:w-1/2 lg:py-6 lg:pl-10">
+              <h2 className="inline-block rounded bg-indigo-100 px-2 py-1 text-xs font-medium tracking-widest text-indigo-600">
                 {item.brand}
               </h2>
-              <h1 className="flex text-gray-900 text-3xl title-font font-medium mb-1 dark:text-gray-200">
+              <h1 className="title-font mb-1 flex text-3xl font-medium text-gray-900">
                 {item.name}
-                <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-400 space-x-3 text-xl">
-                  <a className="text-gray-500 cursor-pointer">
+                <span className="ml-3 flex space-x-3 border-l-2 border-gray-400 py-2 pl-3 text-xl">
+                  <a className="cursor-pointer text-gray-500">
                     <FaFacebookF />
                   </a>
-                  <a className="text-gray-500 cursor-pointer">
+                  <a className="cursor-pointer text-gray-500">
                     <FaTwitter />
                   </a>
-                  <a className="text-gray-500 cursor-pointer">
+                  <a className="cursor-pointer text-gray-500">
                     <FaInstagram />
                   </a>
                 </span>
               </h1>
 
-              <div className="flex mb-4">
-                <span className="flex items-center text-indigo-500 text-2xl space-x-1">
+              <div className="mb-4 flex">
+                <span className="flex items-center space-x-1 text-2xl text-indigo-500">
                   <FaStar />
                   <FaStar />
                   <FaStar />
                   <FaStar />
                   <FaRegStar />
-                  <span className="text-gray-600 ml-3 text-sm">4 Reviews</span>
+                  <span className="ml-3 text-sm text-gray-600">4 Reviews</span>
                 </span>
               </div>
-              <p className="leading-relaxed font-light mb-5 dark:text-gray-300">
+              <p className="mb-5 font-light leading-relaxed">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
                 est voluptatum sit culpa itaque, libero, ducimus doloribus
                 aliquid beatae perferendis velit commodi mollitia quis odit
@@ -81,25 +83,25 @@ export default function Item() {
                 rerum mollitia a assumenda commodi natus laboriosam obcaecati,
                 iste accusamus!
               </p>
-              <div className="flex mb-5 dark:text-gray-300">
+              <div className="mb-5 flex">
                 <span className="mr-3">Color</span>
-                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                <button className="h-6 w-6 rounded-full border-2 border-gray-300 focus:outline-none"></button>
+                <button className="ml-1 h-6 w-6 rounded-full border-2 border-gray-300 bg-gray-700 focus:outline-none"></button>
+                <button className="ml-1 h-6 w-6 rounded-full border-2 border-gray-300 bg-indigo-500 focus:outline-none"></button>
               </div>
               <div className="flex">
-                <span className="title-font font-medium text-2xl text-gray-900 dark:text-gray-400">
+                <span className="title-font text-2xl font-medium text-gray-900">
                   {formatCurrency(item.price)}
                 </span>
                 {quantity > 0 ? (
-                  <button className="flex ml-auto text-white bg-indigo-600 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-2">
+                  <button className="ml-auto mr-2 flex items-center rounded-lg bg-indigo-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
                     Added to cart
                   </button>
                 ) : (
                   <button
                     type="button"
                     onClick={() => addToCart(item.id)}
-                    className="flex ml-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-2">
+                    className="ml-auto mr-2 flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
                     <FaShoppingCart />
                     &nbsp;Add to Cart
                   </button>
@@ -107,13 +109,13 @@ export default function Item() {
                 <button
                   data-tooltip-target="tooltip-right"
                   data-tooltip-placement="right"
-                  className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  className="ml-4 inline-flex h-10 w-10 items-center justify-center rounded-full border-0 bg-gray-200 p-0 text-gray-500">
                   <FaHeart />
                 </button>
                 <div
                   id="tooltip-right"
                   role="tooltip"
-                  className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
+                  className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm">
                   Tooltip on right
                   <div className="tooltip-arrow" data-popper-arrow></div>
                 </div>
