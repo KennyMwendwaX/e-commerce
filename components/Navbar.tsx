cfad10b0e87faf9212928155b922999ea794402b
IconicDexter <kennymwendwa67@gmail.com>
2023-06-17 12:43:30 +0300

    Added prisma schema for the db

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { FaSearch, FaBars } from "react-icons/fa";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
@@ -21,7 +21,7 @@ export default function Navbar() {
     setSearchValue(e.target.value);
   };
 
-  function fetchItems() {
+  const fetchItems = useCallback(() => {
     fetch("http://localhost:3000/api/items")
       .then((res) => res.json())
       .then((data) => {
@@ -51,11 +51,11 @@ export default function Navbar() {
 
         setFilteredItems(searchResults);
       });
-  }
+  }, [searchValue, setFilteredItems]);
 
   useEffect(() => {
     fetchItems();
-  }, [searchValue]);
+  }, [fetchItems, searchValue]);
 
   useEffect(() => {
     if (filteredItems != null && filteredItems.length > 0) {
@@ -67,11 +67,11 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="bg-gray-200 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0">
-        <div className="container flex flex-wrap items-center justify-between mx-auto">
+      <nav className="fixed left-0 top-0 z-20 w-full bg-gray-200 px-2 py-2.5 sm:px-4">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
           <Link href="/" className="flex items-center">
-            <Image src={Logo} className="h-6 mr-3 sm:h-9" alt="Iconic Logo" />
-            <span className="self-center text-gray-800 text-xl tracking-tight font-mono font-semibold whitespace-nowrap">
+            <Image src={Logo} className="mr-3 h-6 sm:h-9" alt="Iconic Logo" />
+            <span className="self-center whitespace-nowrap font-mono text-xl font-semibold tracking-tight text-gray-800">
               Iconic
             </span>
           </Link>
@@ -79,21 +79,21 @@ export default function Navbar() {
             <button
               type="button"
               onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-              className="text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 rounded-lg text-sm p-2.5 mr-4">
+              className="mr-4 rounded-lg p-2.5 text-sm text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300">
               {theme === "light" ? (
-                <BsMoonStarsFill className="w-6 h-6" />
+                <BsMoonStarsFill className="h-6 w-6" />
               ) : (
-                <BsSunFill className="w-6 h-6" />
+                <BsSunFill className="h-6 w-6" />
               )}
               <span className="sr-only">Toggle dark mode</span>
             </button>
             <div className="relative hidden md:block">
               <Link
                 href="/cart"
-                className="text-gray-700 text-3xl cursor-point">
+                className="cursor-point text-3xl text-gray-700">
                 <HiOutlineShoppingCart />
                 {cartQuantity > 0 && (
-                  <span className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-blue-600 rounded-full -top-2 -right-2">
+                  <span className="absolute -right-2 -top-2 inline-flex h-5 w-5 items-center justify-center rounded-full bg-blue-600 text-xs font-bold text-white">
                     {cartQuantity}
                   </span>
                 )}
@@ -105,14 +105,14 @@ export default function Navbar() {
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="md:hidden text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 rounded-lg text-sm p-2.5 mr-1">
+              className="mr-1 rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 md:hidden">
               <FaSearch />
               <span className="sr-only">Search</span>
             </button>
             <button
               data-collapse-toggle="navbar-search"
               type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
               aria-controls="navbar-search"
               aria-expanded="false">
               <span className="sr-only">Open menu</span>
@@ -121,10 +121,10 @@ export default function Navbar() {
           </div>
           {/* Search bar */}
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
             id="navbar-search">
             <div className="relative hidden md:block">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-500 pointer-events-none">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-gray-500">
                 <FaSearch />
                 <span className="sr-only">Search icon</span>
               </div>
@@ -132,7 +132,7 @@ export default function Navbar() {
                 type="text"
                 id="search-navbar"
                 onChange={handleChange}
-                className="block w-full p-2 pl-10 text-sm text-gray-800 border border-gray-500 focus:outline-none focus:border-blue-600 rounded-lg bg-gray-50"
+                className="block w-full rounded-lg border border-gray-500 bg-gray-50 p-2 pl-10 text-sm text-gray-800 focus:border-blue-600 focus:outline-none"
                 placeholder="Search..."
               />
             </div>
