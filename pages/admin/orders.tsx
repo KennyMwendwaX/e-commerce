4cd30cd4f953af669432afc2a33b9e2662d9a52e
IconicDexter <kennymwendwa67@gmail.com>
2023-06-21 13:30:55 +0300

    Added /admin pages

---
+++ pages/admin/orders.tsx
@@ -0,0 +1,7 @@
+export default function Orders() {
+  return (
+    <>
+      <div className="container mx-auto px-5 pb-5 pt-20">orders</div>
+    </>
+  );
+}
---
+++ pages/admin/products.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Products() {
+  return (
+    <>
+      <div className="container mx-auto px-5 pb-5 pt-20">products</div>
+    </>
+  );
+}
