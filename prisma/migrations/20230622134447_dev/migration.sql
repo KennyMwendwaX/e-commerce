7f7fd1d8b05a3f19b5ed433c0cf92dfa3254f103
IconicDexter <kennymwendwa67@gmail.com>
2023-06-22 16:45:22 +0300

    Updated User schema and migrated to db

--- prisma/migrations/20230622134447_dev/migration.sql
+++ prisma/migrations/20230622134447_dev/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
