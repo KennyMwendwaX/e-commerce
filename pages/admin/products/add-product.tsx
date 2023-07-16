c18faf21da95bed1e36b5ad63fb74d6d51aa95cf
IconicDexter <kennymwendwa67@gmail.com>
2023-07-16 11:41:33 +0300

    Added file input on add-product page

--- pages/admin/products/add-product.tsx
+++ pages/admin/products/add-product.tsx
@@ -1,9 +1,62 @@
 import SideLayout from "@/components/SideLayout";
+import axios from "axios";
+import { useState } from "react";
 
 export default function AddProduct() {
+  const [uploading, setUploading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return; // Guard clause
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleUpload = async () => {
+    setUploading(true);
+    try {
+      if (!selectedFile) return;
+      const formData = new FormData();
+      formData.append("uploadedFile", selectedFile);
+
+      const res = await axios.post("/api/upload", formData);
+      if (res.status === 201) {
+        // change route to products
+      }
+    } catch (error: any) {
+      console.log(error.response?.data);
+    }
+    setUploading(false);
+  };
   return (
     <SideLayout>
-      <div>Add Product</div>
+      <main className="h-auto p-4 pt-20 md:ml-64">
+        <div className="text-xl">Add Product</div>
+        <div className="mt-5">
+          <form onSubmit={handleUpload}>
+            <div className="mb-2 text-sm font-medium">Add Product image</div>
+            <input
+              type="file"
+              className="text-grey-500 cursor-pointer rounded-lg border-2 border-gray-900 bg-gray-50 pr-40 text-sm file:mr-5 file:border-0 file:bg-gray-900
+             file:px-6 file:py-2 file:text-sm file:font-medium file:text-white hover:file:cursor-pointer"
+              onChange={handleFileChange}
+              required
+            />
+            <p className="mt-1 text-xs text-gray-500">
+              IMG, PNG, JPG, JPEG, WEBP.
+            </p>
+            <button
+              type="submit"
+              disabled={uploading}
+              style={{ opacity: uploading ? ".5" : "1" }}
+              className="mt-4 w-32 rounded bg-red-600 py-2 text-center text-white">
+              {uploading ? "Uploading.." : "Upload"}
+            </button>
+          </form>
+        </div>
+      </main>
     </SideLayout>
   );
 }
--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,6 +1,8 @@
 import SideLayout from "@/components/SideLayout";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { HiPlus } from "react-icons/hi";
 
 export default function Index() {
   const router = useRouter();
@@ -13,7 +15,15 @@ export default function Index() {
   });
   return (
     <SideLayout>
-      <main className="h-auto p-4 pt-20 md:ml-64">Products</main>
+      <main className="h-auto p-4 pt-20 md:ml-64">
+        <div className="text-xl">Products</div>
+        <Link
+          href="/admin/products/add-product"
+          className="mt-5 inline-flex items-center rounded-lg border border-gray-600 bg-transparent px-10 py-2.5 text-sm font-medium text-gray-700 hover:border-gray-800 hover:bg-gray-800 hover:text-gray-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200">
+          <HiPlus className="mr-1 h-5 w-5" />
+          Add Product
+        </Link>
+      </main>
     </SideLayout>
   );
 }
