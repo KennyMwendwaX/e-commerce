ee6e9fcf4e0fcc3d33ea83ea91d73ce19a8d8d78
IconicDexter <kennymwendwa67@gmail.com>
2023-07-16 13:27:29 +0300

    Deigned the add product form

--- pages/admin/products/add-product.tsx
+++ pages/admin/products/add-product.tsx
@@ -1,6 +1,7 @@
 import SideLayout from "@/components/SideLayout";
 import axios from "axios";
 import { useState } from "react";
+import { BiCloudUpload } from "react-icons/bi";
 
 export default function AddProduct() {
   const [uploading, setUploading] = useState(false);
@@ -35,112 +36,123 @@ export default function AddProduct() {
       <main className="h-auto p-4 pt-20 md:ml-64">
         <div className="text-xl">Add Product</div>
         <div className="mt-5">
-          <form onSubmit={handleProductUpload}>
-            <label
-              htmlFor="file"
-              className="mb-2 block text-sm font-medium text-gray-900">
-              Product image
-            </label>
-            <input
-              type="file"
-              id="file"
-              className="text-grey-500 w-full cursor-pointer rounded-lg border-2 border-gray-900 bg-gray-50 pr-40 text-sm file:mr-5 file:border-0 file:bg-gray-900
-             file:px-6 file:py-2 file:text-sm file:font-medium file:text-white hover:file:cursor-pointer"
-              onChange={handleFileChange}
-              required
-            />
-            <p className="mt-1 text-xs text-gray-500">
-              IMG, PNG, JPG, JPEG, WEBP.
-            </p>
-            <div className="mb-6">
+          <form
+            className="mx-auto w-2/3 space-y-6"
+            onSubmit={handleProductUpload}>
+            <div className="flex w-full items-center justify-center">
+              <label
+                htmlFor="dropzone-file"
+                className="flex h-72 w-1/2 cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100">
+                <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                  <BiCloudUpload className="mb-4 h-12 w-12 text-gray-500" />
+                  <p className="mb-2 text-sm text-gray-500">
+                    <span className="font-semibold">
+                      Click to upload product image
+                    </span>{" "}
+                    or drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500">
+                    IMG, PNG, JPG, JPEG or WEBP (MAX. 800x400px)
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div>
+
+            <div>
               <label
                 htmlFor="name"
                 className="mb-2 block text-sm font-medium text-gray-900">
                 Product Name
               </label>
               <input
-                type="name"
+                type="text"
                 id="name"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
                 placeholder="name"
                 required
               />
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="brand"
-                className="mb-2 block text-sm font-medium text-gray-900">
-                Product Brand
-              </label>
-              <input
-                type="brand"
-                id="brand"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
-                placeholder="brand"
-                required
-              />
+            <div className="grid md:grid-cols-2 md:gap-6">
+              <div>
+                <label
+                  htmlFor="brand"
+                  className="mb-2 block text-sm font-medium text-gray-900">
+                  Product Brand
+                </label>
+                <input
+                  type="text"
+                  id="brand"
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
+                  placeholder="brand"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="category"
+                  className="mb-2 block text-sm font-medium text-gray-900">
+                  Product Category
+                </label>
+                <input
+                  type="text"
+                  id="category"
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
+                  placeholder="category"
+                  required
+                />
+              </div>
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="category"
-                className="mb-2 block text-sm font-medium text-gray-900">
-                Product Category
-              </label>
-              <input
-                type="category"
-                id="category"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
-                placeholder="category"
-                required
-              />
+
+            <div className="grid md:grid-cols-2 md:gap-6">
+              <div>
+                <label
+                  htmlFor="price"
+                  className="mb-2 block text-sm font-medium text-gray-900">
+                  Product Price
+                </label>
+                <input
+                  type="number"
+                  id="price"
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
+                  placeholder="price"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="quantity"
+                  className="mb-2 block text-sm font-medium text-gray-900">
+                  Product Quantity
+                </label>
+                <input
+                  type="number"
+                  id="quantity"
+                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
+                  placeholder="quantity"
+                  required
+                />
+              </div>
             </div>
-            <div className="mb-6">
+
+            <div>
               <label
                 htmlFor="description"
                 className="mb-2 block text-sm font-medium text-gray-900">
                 Product Description
               </label>
-              <input
-                type="description"
+              <textarea
                 id="description"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
                 placeholder="description"
-                required
-              />
+                required></textarea>
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="price"
-                className="mb-2 block text-sm font-medium text-gray-900">
-                Product Price
-              </label>
-              <input
-                type="price"
-                id="price"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
-                placeholder="price"
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="Quantity"
-                className="mb-2 block text-sm font-medium text-gray-900">
-                Product Quantity
-              </label>
-              <input
-                type="Quantity"
-                id="Quantity"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:border-gray-900"
-                placeholder="Quantity"
-                required
-              />
-            </div>
+
             <button
               type="submit"
               disabled={uploading}
               style={{ opacity: uploading ? ".5" : "1" }}
-              className="mt-4 w-32 rounded bg-red-600 py-2 text-center text-white">
+              className="mt-5 inline-flex items-center rounded-lg border bg-gray-800 px-10 py-2.5 text-sm font-medium text-white hover:bg-gray-600 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200">
               {uploading ? "Uploading..." : "Upload"}
             </button>
           </form>
