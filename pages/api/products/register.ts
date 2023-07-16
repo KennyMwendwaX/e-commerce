67791afcdedf7a1d5eba319abb2e7c1b0580581f
IconicDexter <kennymwendwa67@gmail.com>
2023-07-16 15:04:26 +0300

    disables bodyparser on /products/register api endpoint

--- pages/api/products/register.ts
+++ pages/api/products/register.ts
@@ -1,5 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
