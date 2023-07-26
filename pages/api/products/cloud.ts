778ab70ce67d3953fdf44021a4ee8b1d6e0ccf0a
IconicDexter <kennymwendwa67@gmail.com>
2023-07-26 21:02:14 +0300

    cloudinary api route

---
+++ pages/api/products/cloud.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
