0902e6d9da0ff8fa5912eb2983353188e1f1f8dc
IconicDexter <kennymwendwa67@gmail.com>
2023-07-27 16:18:45 +0300

    Added cloudinary config to cloud.ts

--- pages/api/products/cloud.ts
+++ pages/api/products/cloud.ts
@@ -1,5 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  secure: true,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export const config = {
   api: {
