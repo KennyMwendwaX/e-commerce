b8ff613a53426c40e443a1d5ffdd63cd23676821
IconicDexter <kennymwendwa67@gmail.com>
2023-07-27 16:00:11 +0300

    npm i cloudinary

--- pages/api/products/cloud.ts
+++ pages/api/products/cloud.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
+  if (req.method !== "POST")
     return res.status(405).json({ message: "Method Not Allowed" });
-  }
 }
