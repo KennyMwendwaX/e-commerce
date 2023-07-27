fd7cf64327d789587e9639e805b6ccd6973a5caf
IconicDexter <kennymwendwa67@gmail.com>
2023-07-27 16:14:21 +0300

    integrated formidable to cloud.ts

--- pages/api/products/cloud.ts
+++ pages/api/products/cloud.ts
@@ -1,4 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import formidable from "formidable";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const uploadFile = async (
+  req: NextApiRequest,
+  saveLocally?: boolean
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  const options: formidable.Options = {};
+  options.maxFileSize = 4000 * 1024 * 1024;
+  const form = formidable(options);
+
+  return new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
+    (resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          // Access the fields under the "product" key
+          const productFields = JSON.parse(fields.product[0]);
+          resolve({ fields: productFields, files });
+        }
+      });
+    }
+  );
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -6,4 +36,17 @@ export default async function handler(
 ) {
   if (req.method !== "POST")
     return res.status(405).json({ message: "Method Not Allowed" });
+
+  try {
+    const { fields, files } = await uploadFile(req, true);
+
+    const myFiles = files.picture as formidable.File[];
+    const file = myFiles[0];
+
+    const { name, brand, category, price, quantity, description } = fields;
+    console.log(fields);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
