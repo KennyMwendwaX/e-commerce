8135ab617f00f3be036db3057bdc8ec0ffd9a517
IconicDexter <kennymwendwa67@gmail.com>
2023-08-01 17:50:32 +0300

    get session on products/delete/id api route

--- pages/products/delete/[id].ts
+++ pages/products/delete/[id].ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(
   req: NextApiRequest,
@@ -6,4 +8,9 @@ export default async function handler(
 ) {
   if (req.method !== "DELETE")
     return res.status(405).json({ message: "Method Not Allowed" });
+
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session)
+    return res.status(401).json({ message: "User must be logged in" });
 }
