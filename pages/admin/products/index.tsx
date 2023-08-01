f606b5ea488a05048583b8aac39bdb27a72fc3c2
IconicDexter <kennymwendwa67@gmail.com>
2023-08-01 19:46:11 +0300

    Minor UI changes to the admin/products page

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -44,60 +44,67 @@ export default function Index() {
           Add Product
         </Link>
 
-        <table className="text-left text-sm text-gray-500">
-          <thead className="border-b border-gray-400 text-sm text-gray-900">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Image</span>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Product
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Quantity
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-transparent">
-            {items.map((item) => (
-              <tr key={item.id} className="border-b border-gray-400">
-                <td className="w-32 p-4">
-                  <Image
-                    width={128}
-                    height={128}
-                    src={item.imgUrl}
-                    alt={item.name}
-                  />
-                </td>
-                <td className="px-6 py-4 font-semibold text-gray-700">
-                  {item.name}
-                </td>
-                <td className="px-6 py-4">
-                  <div className="inline-flex items-center border border-gray-500 bg-transparent font-light">
-                    <span className="sr-only">Quantity button</span>
-                    <span className="bg-transparent px-5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none">
-                      {item.quantity}
-                    </span>
-                  </div>
-                </td>
-                <td className="px-6 py-4 font-semibold text-gray-900">
-                  {formatCurrency(item.price)}
-                </td>
-                <td className="px-6 py-4">
-                  <button className="font-medium text-red-600 hover:underline">
-                    Delete
-                  </button>
-                </td>
+        {items.length > 0 ? (
+          <table className="mt-2 text-left text-sm text-gray-500">
+            <thead className="border-b border-gray-400 text-sm text-gray-900">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  <span className="sr-only">Image</span>
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Product
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Quantity
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="bg-transparent">
+              {items.map((item) => (
+                <tr key={item.id} className="border-b border-gray-400">
+                  <td className="w-32 cursor-pointer p-4">
+                    <Image
+                      width={128}
+                      height={128}
+                      src={item.imgUrl}
+                      alt={item.name}
+                    />
+                  </td>
+                  <td className="px-6 py-4 font-semibold text-gray-700">
+                    {item.name}
+                  </td>
+                  <td className="px-6 py-4">
+                    <div className="inline-flex items-center border border-gray-500 bg-transparent font-light">
+                      <span className="sr-only">Quantity button</span>
+                      <span className="bg-transparent px-5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none">
+                        {item.quantity}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 font-semibold text-gray-900">
+                    {formatCurrency(item.price)}
+                  </td>
+                  <td className="px-6 py-4">
+                    <button className="font-medium text-red-600 hover:underline">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className="mt-2">
+            You have no products available, click the Add Product button to add
+            a product.
+          </div>
+        )}
       </main>
     </SideLayout>
   );
