eadb305596a8d46eb0620b28013092f8f105fb7d
IconicDexter <kennymwendwa67@gmail.com>
2023-08-02 22:04:58 +0300

    Renamed function fetchItems to fetchProduct on admin/products

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -18,21 +18,22 @@ export default function Index() {
     },
   });
 
-  const [items, setItems] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    async function fetchItems() {
+    async function fetchProducts() {
       const response = await fetch("/api/products/user-products");
       const data = await response.json();
-      // Sort items by createdAt in descending order
-      const sortedItems: Product[] = data.sort(
+      // Sort products by createdAt in descending order
+      const sortedProducts: Product[] = data.sort(
         (a: Product, b: Product) =>
           new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
       );
-      setItems(sortedItems);
+      setProducts(sortedProducts);
     }
-    fetchItems();
+    fetchProducts();
   }, []);
+
   return (
     <SideLayout>
       <main className="h-auto p-4 pt-20 md:ml-64">
@@ -44,7 +45,7 @@ export default function Index() {
           Add Product
         </Link>
 
-        {items.length > 0 ? (
+        {products.length > 0 ? (
           <table className="mt-2 text-left text-sm text-gray-500">
             <thead className="border-b border-gray-400 text-sm text-gray-900">
               <tr>
@@ -66,29 +67,29 @@ export default function Index() {
               </tr>
             </thead>
             <tbody className="bg-transparent">
-              {items.map((item) => (
-                <tr key={item.id} className="border-b border-gray-400">
+              {products.map((product) => (
+                <tr key={product.id} className="border-b border-gray-400">
                   <td className="w-32 cursor-pointer p-4">
                     <Image
                       width={128}
                       height={128}
-                      src={item.imgUrl}
-                      alt={item.name}
+                      src={product.imgUrl}
+                      alt={product.name}
                     />
                   </td>
                   <td className="px-6 py-4 font-semibold text-gray-700">
-                    {item.name}
+                    {product.name}
                   </td>
                   <td className="px-6 py-4">
                     <div className="inline-flex items-center border border-gray-500 bg-transparent font-light">
                       <span className="sr-only">Quantity button</span>
                       <span className="bg-transparent px-5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none">
-                        {item.quantity}
+                        {product.quantity}
                       </span>
                     </div>
                   </td>
                   <td className="px-6 py-4 font-semibold text-gray-900">
-                    {formatCurrency(item.price)}
+                    {formatCurrency(product.price)}
                   </td>
                   <td className="px-6 py-4">
                     <button className="font-medium text-red-600 hover:underline">
