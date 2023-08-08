aca8083e3285e3424c3be63005e7f7b8e636c1a0
IconicDexter <kennymwendwa67@gmail.com>
2023-08-08 16:50:08 +0300

    Added head and meta tags on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,28 @@ import TopDeals from "../components/TopDeals";
 import PopularCategories from "../components/PopularCategories";
 import PopularBrands from "../components/PopularBrands";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-5 pb-5 pt-20">
-      <TopDeals />
-      <div className="mb-10">
-        <PopularCategories />
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container mx-auto px-5 pb-5 pt-20">
+        <TopDeals />
+        <div className="mb-10">
+          <PopularCategories />
+        </div>
+        <div className="mb-10">
+          <PopularBrands />
+        </div>
+        <Footer />
       </div>
-      <div className="mb-10">
-        <PopularBrands />
-      </div>
-      <Footer />
-    </div>
+    </>
   );
 }
