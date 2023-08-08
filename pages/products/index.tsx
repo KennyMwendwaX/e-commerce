ed22b22972cc0ba057a8a479857dc71d8cafef12
IconicDexter <kennymwendwa67@gmail.com>
2023-08-08 16:57:47 +0300

    Added head and meta tags on products page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import StoreItem from "@/components/StoreItem";
 import { useSearchContext } from "@/context/SearchContext";
 import { Product } from "@/types/ProductTypes";
+import Head from "next/head";
 
 export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -32,28 +33,37 @@ export default function Products() {
   }, [filteredItems]);
 
   return (
-    <div className="body-font text-gray-600">
-      <div className="container mx-auto mt-4 px-5 pb-5 pt-20">
-        <div className="-m-4 flex flex-wrap">
-          {searchResults ? (
-            <>
-              {searchResults.map((item) => (
-                <div className="w-full p-4 md:w-1/2 lg:w-1/4" key={item.id}>
-                  <StoreItem {...item} />
-                </div>
-              ))}
-            </>
-          ) : (
-            <>
-              {products.map((item) => (
-                <div className="w-full p-4 md:w-1/2 lg:w-1/4" key={item.id}>
-                  <StoreItem {...item} />
-                </div>
-              ))}
-            </>
-          )}
+    <>
+      <Head>
+        <title>Products</title>
+        <meta name="description" content="Products page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="body-font text-gray-600">
+        <div className="container mx-auto mt-4 px-5 pb-5 pt-20">
+          <div className="-m-4 flex flex-wrap">
+            {searchResults ? (
+              <>
+                {searchResults.map((item) => (
+                  <div className="w-full p-4 md:w-1/2 lg:w-1/4" key={item.id}>
+                    <StoreItem {...item} />
+                  </div>
+                ))}
+              </>
+            ) : (
+              <>
+                {products.map((item) => (
+                  <div className="w-full p-4 md:w-1/2 lg:w-1/4" key={item.id}>
+                    <StoreItem {...item} />
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
