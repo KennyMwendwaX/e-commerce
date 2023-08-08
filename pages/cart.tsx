b88e56f518c8f924737bc3ce2d172b0c145f1a86
IconicDexter <kennymwendwa67@gmail.com>
2023-08-08 16:51:07 +0300

    Added head and meta tags on cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -3,38 +3,48 @@ import CartComponent from "../components/CartComponent";
 import CartSummary from "../components/CartSummary";
 import { useCart } from "../context/CartContext";
 import { FaArrowLeft } from "react-icons/fa";
+import Head from "next/head";
 
 export default function Cart() {
   const { cartQuantity } = useCart();
 
   return (
-    <div className="container mx-auto px-5 py-20">
-      {cartQuantity > 0 ? (
-        <>
-          <div className="text-xl font-medium">My Cart</div>
-          <div className="flex-basis flex">
-            <CartComponent />
-            <CartSummary />
-          </div>
-        </>
-      ) : (
-        <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
-          <div className="mx-auto max-w-screen-sm text-center">
-            <div className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
-              No items in the cart
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container mx-auto px-5 py-20">
+        {cartQuantity > 0 ? (
+          <>
+            <div className="text-xl font-medium">My Cart</div>
+            <div className="flex-basis flex">
+              <CartComponent />
+              <CartSummary />
             </div>
-            <div className="mb-4 font-mono text-lg font-light text-gray-500">
-              Please add items to the cart. You&apos;ll find lots of products to
-              explore on the home page.
+          </>
+        ) : (
+          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+            <div className="mx-auto max-w-screen-sm text-center">
+              <div className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+                No items in the cart
+              </div>
+              <div className="mb-4 font-mono text-lg font-light text-gray-500">
+                Please add items to the cart. You&apos;ll find lots of products
+                to explore on the home page.
+              </div>
+              <Link
+                href="/"
+                className="my-4 inline-flex items-center rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                <FaArrowLeft /> &nbsp; Back to Homepage
+              </Link>
             </div>
-            <Link
-              href="/"
-              className="my-4 inline-flex items-center rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
-              <FaArrowLeft /> &nbsp; Back to Homepage
-            </Link>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
